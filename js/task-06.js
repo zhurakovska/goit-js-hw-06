b5117f4a715b808e89bcbecf34abe89333ed9f3f
zhurakovska <n.sinawka@gmail.com>
2023-06-24 18:50:23 +0300

    done task 6

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const inputEl = document.getElementById('validation-input')
+
+const onInputCheckBlur = ({target}) => {
+    const expectedLenght = parseInt(target.dataset.length)
+    const inputLength = target.value.length;
+
+    console.log(expectedLenght)
+    
+    if (expectedLenght === inputLength) {
+        target.classList.add('valid');
+        target.classList.remove('invalid');
+      } else {
+        target.classList.remove('valid');
+        target.classList.add('invalid');
+      }
+}
+
+inputEl.addEventListener('blur',onInputCheckBlur)

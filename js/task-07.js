0e0b4346fef045b2d87b54ab328f4df091a4e71a
zhurakovska <n.sinawka@gmail.com>
2023-06-24 19:30:05 +0300

    done task 7

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const sizeControlEl = document.getElementById('font-size-control')
+const textEl = document.getElementById('text')
+
+const onChangeSizeInput = ({target}) => {
+    const inputSizeEl = target.value
+    textEl.style.fontSize = inputSizeEl + 'px'
+}
+
+sizeControlEl.addEventListener('input', onChangeSizeInput)

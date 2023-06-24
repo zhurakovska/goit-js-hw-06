4a241655d10575af73c59f013b50c792fd9a6281
zhurakovska <n.sinawka@gmail.com>
2023-06-24 23:54:57 +0300

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,20 @@
+let bgrColor;
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+   bgrColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}`; 
+
+    return bgrColor;
 }
+
+const pageColorEl = document.querySelector('body');
+const changeColorBtnEl = document.querySelector('.change-color')
+const initialNameColorEl = document.querySelector('.color')
+
+const onChangeColorClick = () => {
+  pageColorEl.style.backgroundColor = getRandomHexColor()
+  initialNameColorEl.innerHTML = bgrColor
+}
+
+changeColorBtnEl.addEventListener('click', onChangeColorClick)
